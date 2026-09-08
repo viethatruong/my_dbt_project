f8435a6e13b8a95e6ccfd24d95ea664a33c7a307
chaton912006@yahoo.fr <chaton912006@yahoo.fr>
2026-09-08 16:18:20 +0000

    fix: modification dbt_project, schema.yml

--- models/mart/mrt_monthly_order_report.sql
+++ models/mart/mrt_monthly_order_report.sql
@@ -1,12 +1,9 @@
 WITH monthly_users_recap AS (
-
-
 SELECT DATE_TRUNC(order_date,month) AS order_month,
 COUNT(DISTINCT user_name) AS total_monthly_users
 FROM {{ source('sales_database', 'order')}}
 GROUP BY 1
 ORDER BY total_monthly_users DESC
-
 
 ), total_monthly_user_from_jawa_timur AS (
 SELECT DATE_TRUNC(order_date,month) AS order_month,
@@ -16,23 +13,18 @@ LEFT JOIN {{ source('sales_database', 'user')}} AS user ON user.user_name = orde
 WHERE user.customer_state LIKE '%JAWA%TIMUR%'
 GROUP BY order_month
 
-
 ), monthly_orders_recap AS (
-
-
-SELECT DATE_TRUNC(order_date,month) AS order_months,
+SELECT DATE_TRUNC(order_date,month) AS order_month,  -- Changé: order_months → order_month
 COUNT(order_id) AS total_monthly_orders
 FROM {{ source('sales_database', 'order')}}
 GROUP BY order_month
 
-
 ), shipping_cost AS (
-
-
 SELECT shipping_cost
 FROM sales_database.order_item
 WHERE price > 7000
 )
+
 SELECT u.order_month,
 COALESCE(u.total_monthly_users,0) AS nb_users_monthly,
 COALESCE(jt.total_monthly_users_from_jawa_timur,0) AS total_monthly_user,
