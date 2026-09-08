ce369bd24c541f45c398fc3276eb387b316f03e3
viethatruong <viethakugeler@gmail.com>
2026-09-08 16:01:14 +0200

    my second commit

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -1,10 +1,10 @@
 select
-    order_id,
-    user_name as user_id,
-    order_status,
-    DATETIME(order_date, "Europe/Paris") AS order_created_at,
-    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at,
-    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at,
-    DATETIME(delivered_date, "Europe/Paris") AS delivered_at,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery
+    order_id as order_y,
+    user_name as user_id_y,
+    order_status as order_status_y,
+    DATETIME(order_date, "Europe/Paris") AS order_created_at_y,
+    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_y,
+    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_y,
+    DATETIME(delivered_date, "Europe/Paris") AS delivered_at_y,
+    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_y
 from {{ source('sales_database', 'order') }}
